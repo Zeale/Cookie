32de19ae1a580575df05661998f83037af92da96
Zeale <a@infinityminers.com>
2017-06-14 00:25:04 -0500

    Added "Notes" section

--- Cookie.bat
+++ Cookie.bat
@@ -1,2 +1,13 @@
 @echo off
+goto Head
+
+Welcome to COOKIE. A couple of notes here before the program starts.
+
+1. If you find any characters in this document that appear as boxes, (specifically this:  character),
+   then don't worry; that's the escape character. It's used to change the foreground and background color
+   of text, along with some other things such as underlining text, or making it bold. You can check out
+   'https://stackoverflow.com/a/38617204' for a list of color codes and their outputs.
+
+:Head
+:: Display our little title
 echo Welcome to [91;1mCookie[0m!
