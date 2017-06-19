bd9d53f4d656dfd47d3f3cea707fc09bdb39b359
Zeale <a@infinityminers.com>
2017-06-19 13:30:26 -0500

    Update notes

--- Cookie.bat
+++ Cookie.bat
@@ -3,8 +3,8 @@ GOTO Head
 
 Welcome to COOKIE. A couple of notes here before the program starts.
 
-1. If you find any characters in this document that appear as boxes, (specifically this:  character),
-	then don't worry; that's the escape character. It's used to change the foreground and background color
+1. If you find any characters in this document that appear as boxes (or red circles, if you're on github), (specifically this:  character),
+	then don't worry; that's an escape character, (literally the character "Escape"). It's used to change the foreground and background color
 	of text, along with some other things such as underlining text, or making it bold. You can check out
 	'https://stackoverflow.com/a/38617204' for a list of color codes and their outputs.
 
